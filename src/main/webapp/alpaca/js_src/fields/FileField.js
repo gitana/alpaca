0fe7a11c93a69c093dc61a8d4474ed98d7787a59
drq <quyong2000@yahoo.com>
2011-05-02 16:43:17 -0400

    fixed minor issue with field name for file control field.

--- src/main/webapp/alpaca/js_src/fields/FileField.js
+++ src/main/webapp/alpaca/js_src/fields/FileField.js
@@ -82,6 +82,6 @@
         }
     });
     
-    Alpaca.registerTemplate("controlFieldFile", '<input type="file" id="${id}" {{if options.size}}size="${options.size}"{{/if}} {{if options.readonly}}readonly="readonly"{{/if}} {{if options.formName}}name="${options.formName}"{{/if}} {{each(i,v) options.data}}data-${i}="${v}"{{/each}}/>');
+    Alpaca.registerTemplate("controlFieldFile", '<input type="file" id="${id}" {{if options.size}}size="${options.size}"{{/if}} {{if options.readonly}}readonly="readonly"{{/if}} {{if options.name}}name="${options.name}"{{/if}} {{each(i,v) options.data}}data-${i}="${v}"{{/each}}/>');
     Alpaca.registerFieldClass("file", Alpaca.Fields.FileField);
 })(jQuery);
