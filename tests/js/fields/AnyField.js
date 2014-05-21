00caeae7684e9df7d3d331d89258340c23ad2d7a
William Yaworsky <wj.px01@gmail.com>
2014-05-21 13:44:35 -0400

    AnyField test sort of passesish

--- tests/js/fields/AnyField.js
+++ tests/js/fields/AnyField.js
@@ -1,11 +1,19 @@
 (function($) {
 
-    module("fields: any");
+    module("fields: any", {
+        "setup": function() {
+            $("#qunit-fixture").append('<div id="any-fixture"></div>');
+        },
+        "teardown": function() {
+            $("#any-fixture").remove();
+        }
+    });
 
     // Test case 1 : Any field with data and schema.
     test("Any field with data and schema", function() {
         stop();
-        $("#any-1").alpaca({
+        $("#any-fixture").alpaca({
+            "view": "bootstrap-edit",
             "data": "I Love Alpaca Ice Cream!",
             "schema": {
                 "title": "Ice Cream",
@@ -13,10 +21,10 @@
             },
             "postRender": function (renderedField) {
                 expect(4);
-                var inputElem = $('#any-1 input:text');
+                var inputElem = $('#any-fixture input:text');
                 ok(inputElem.length, 'Text input field generated.');
                 equal(inputElem.val(), 'I Love Alpaca Ice Cream!', 'Input field value populated correctly.');
-                var labelElem = $('#any-1 .alpaca-controlfield-label>div');
+                var labelElem = $('#any-fixture .alpaca-control-label');
                 ok(labelElem.length, 'Input field label generated.');
                 equal(labelElem.text(), 'Ice Cream', 'Label text populated correctly.');
                 start();
