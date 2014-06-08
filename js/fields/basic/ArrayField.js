a1ba2ec7c0ea1f140a5d2adeb10fa5b37b2c437d
Poltorak Serguei <poltorak@alsenet.com>
2014-06-09 03:01:32 +0400

    Fix emptying array field
    After deleting everything from array, old values are still saved, since undefined was returned instead of []

--- js/fields/basic/ArrayField.js
+++ js/fields/basic/ArrayField.js
@@ -178,7 +178,7 @@
             // if we're empty and we're also not required, then we hand back undefined
             if (this.children.length === 0 && !this.schema.required)
             {
-                return;
+                return [];
             }
 
             // otherwise, construct an array and had it back
