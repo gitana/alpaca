8f658dc7fa06e5c2917c6f9ad4e32cc63934ccca
uzquiano <mgu1@cornell.edu>
2015-08-01 10:38:00 -0400

    alpaca release build 1.5.13

---
+++ dist/lib/ckeditor/plugins/iframe/lang/eu.js
@@ -0,0 +1,1 @@
+CKEDITOR.plugins.setLang("iframe","eu",{border:"Markoaren ertza ikusi",noUrl:"iframe-aren URLa idatzi, mesedez.",scrolling:"Korritze barrak gaitu",title:"IFrame-aren Propietateak",toolbar:"IFrame"});
