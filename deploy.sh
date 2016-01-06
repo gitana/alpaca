47e3df3b377f1c45411e59b6c780158b640493f0
uzquiano <mgu1@cornell.edu>
2016-01-06 15:42:20 -0500

    Latest scripts

--- deploy.sh
+++ deploy.sh
@@ -83,7 +83,19 @@ git push origin $VERSION
 # STEP 5: NPM
 # This copies essentials into a new directory.
 #
-source deploy-npm.sh
+rm -r npm
+mkdir npm
+cp README.md npm
+cp bower.json npm
+cp -r dist npm
+cp license.txt npm
+cp package.json npm
+cp -r src npm
+cp -r tests npm
+cp -r thirdparty npm
+cd npm
+npm publish --force
+cd ..
 
 
 
