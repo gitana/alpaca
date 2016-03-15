0fcf0ebcc91957a5dfc0d6e6392a498e563d8215
uzquiano <mgu1@cornell.edu>
2016-03-14 23:24:52 -0400

    Fix for table setValue on recursion issue

--- src/js/Field.js
+++ src/js/Field.js
@@ -1073,8 +1073,13 @@
                 // mark that we're refreshed
                 self.refreshed = true;
 
-                if (typeof(_data) !== "undefined") {
-                    self.setValue(_data);
+                // this is apparently needed for objects and arrays
+                if (typeof(_data) !== "undefined")
+                {
+                    if (Alpaca.isObject(_data) || Alpaca.isArray(_data))
+                    {
+                        self.setValue(_data);
+                    }
                 }
 
                 // fire the "ready" event
