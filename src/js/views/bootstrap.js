2e38809fe71497588514f839d41057052cc27e86
uzquiano <mgu1@cornell.edu>
2017-02-14 11:13:43 -0500

    Fix for collapsed logic on bootstrap, https://github.com/gitana/alpaca/issues/230

--- src/js/views/bootstrap.js
+++ src/js/views/bootstrap.js
@@ -164,7 +164,11 @@
             }
 
             // set up container to be collapsible
-            $(containerEl).addClass("collapse in");
+            $(containerEl).addClass("collapse");
+            if (!this.options.collapsed)
+            {
+                $(containerEl).addClass("in");
+            }
 
             // set up legend anchor
             if (!$(anchorEl).attr("data-target")) {
