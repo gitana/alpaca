a2ecbb8c3583fc56ed0ddfe86f6d32320ae25e17
davidepastore <pasdavide@gmail.com>
2017-06-23 14:08:15 +0200

    Add control-checkbox.html for Bootstrap display

--- src/js/HandlebarsTemplateEngine.js
+++ src/js/HandlebarsTemplateEngine.js
@@ -236,6 +236,12 @@
 
     });
 
+    Handlebars.registerHelper("ifIn", function(elem, list, options) {
+        if(list.indexOf(elem) > -1) {
+            return options.fn(this);
+        }
+        return options.inverse(this);
+    });
 
 
     //Handlebars.registerHelper("each", helpers["each"]);
