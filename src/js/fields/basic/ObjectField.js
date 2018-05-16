ff0949beff1669354bf849c46403736166050e5e
eddy99 <chia-cn@163.com>
2018-05-16 16:34:17 +0800

    Update ObjectField.js
    hasbug if index=0

--- src/js/fields/basic/ObjectField.js
+++ src/js/fields/basic/ObjectField.js
@@ -1118,7 +1118,7 @@
             var self = this;
 
             // insert into dom
-            if (!index)
+            if (index==null)
             {
                 // insert first into container
                 $(self.container).prepend(item.containerItemEl);
